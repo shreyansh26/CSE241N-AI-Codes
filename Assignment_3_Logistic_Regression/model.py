ec3b97da9267ee7131a6dfb2768585409d110c2b
Shreyansh <shreyansh.pettswood@gmail.com>
2018-02-18 17:30:36 +0530

    Assignment 3

---
+++ Assignment_3_Logistic_Regression/model.py
@@ -0,0 +1,189 @@
+"""
+model.py
+
+This file implements the Logistic Regression model for classification.
+"""
+
+import numpy as np
+
+
+def sigmoid(x):
+    """
+    Sigmoid function implemented using numpy.
+
+    @x: A numpy array of size n.
+
+    returns: a numpy array with each element has been replaced by its sigmoid.
+    """
+
+    output = None
+
+    ''' WRITE YOUR CODE HERE (use just one line of vectorized code) '''
+    output = 1 / (1 + np.exp(-1*x))
+    ''' END YOUR CODE HERE '''
+
+    return output
+
+
+class LogisticRegression(object):
+    """
+    A binary Logistic Regression model.
+    """
+
+
+    def __init__(self, add_bias = False):
+        """
+        Initialise the model. Here, we only initialise the weights as 'None',
+        as the input size of the model will become apparent only when some
+        training samples will be given to us.
+
+        @add_bias: Whether to add bias.
+        """
+
+        # Initialise model parameters placeholders. Don't need another placeholder
+        # for bias, as it can be incorporated in the weights matrix by adding
+        # another feature in the input vector whose value is always 1.
+        self.weights = None
+
+        # Store the add_bias property too.
+        self.add_bias = add_bias
+
+
+    def loss(self, X_batch, Y_batch):
+        """
+        Loss function for Logistic Regression.
+
+        Calculates the loss and gradients of weights with respect to loss
+        for a batch of the logistic regression model.
+
+        @X_batch: The input for batch, a numpy array of size NxD, where
+                  N is the number of samples and D is the feature vector.
+                  Assume that if 'self.add_bias' was set to True, D will
+                  include a column for bias, with values always 1.
+        @Y_batch: The correct labels for the batch, a numpy array of size N.
+
+        returns: A tuple (loss, gradient).
+        """
+
+        loss, grad = None, None
+
+        ''' START YOUR CODE HERE '''
+
+        # Make prediction - use the sigmoid function and self.weights (use hint.pdf in case you forget the equations involved)
+        pred = sigmoid(X_batch.dot(self.weights))
+        # calculate loss (stored in variable name loss) and gradients (stored in variable name grad)
+        m = Y_batch.shape[0]
+        loss = (-1/m) * np.sum(Y_batch.T*np.log(pred) + (1-Y_batch).T*np.log(1-pred))
+        grad = -1*X_batch.T.dot((Y_batch-pred))
+        ''' END YOUR CODE HERE '''
+
+        return loss, grad
+
+
+    def predict(self, X_batch):
+        """
+        Predict the correct labels for the examples in X_batch.
+
+        Remember: We don't need sigmoid for this. If x > 0 then sigmoid(x) > 0.5, and
+        if x <= 0 then sigmoid(x) <= 0.5.
+
+        @X_batch: The input to predict for. A numpy array of size NxD,
+                  where N is the number of examples, and D is the size of input vector.
+                  Since this will probably be called from outside this class,
+                  we need to chech the @self.add_bias variable and adequately
+                  pass the X_batch.
+
+        returns: A vector of size N which contains 0 or 1 indicating the class label.
+        """
+
+        predict_func = np.vectorize(lambda x: 0 if x < 0 else 1)
+
+        if self.add_bias:
+            score = np.dot(np.hstack((X_batch, np.ones((X_batch.shape[0], 1)))), self.weights)
+        else:
+            score = np.dot(X_batch, self.weights)
+
+        predictions = predict_func(score)
+
+        return predictions
+
+
+    def score(self, X_test, Y_test):
+        """
+        Score the performance of the model with the given test labels.
+
+        @X_test: Test input numpy array of size NxD.
+        @Y_test: Test correct labels numpy array of size N.
+
+        returns: The accuracy of the model. A single float value between 0 and 1.
+        """
+
+        Y_pred = self.predict(X_test)
+        accuracy = 1 - np.float(np.count_nonzero(Y_pred - Y_test)) / X_test.shape[0]
+
+        return accuracy
+
+
+    def train(self,
+              X_train,
+              Y_train,
+              lr = 1e-3,
+              reg_const = 1.0,
+              num_iters = 10000,
+              num_print = 100):
+        """
+        Learn the parameters from the given supervised samples with the given hyper parameters, using
+        stochastic gradient descent.
+
+        @X_train: Examples to learn from. It is a vector of size NxD,
+                  with N being the number of samples, and D being the
+                  number of features. Here, we need to check the @self.add_bias
+                  flag and adequately pad the input vector with ones.
+        @Y_train: Correct labels for X_train. Vector of size N.
+        @lr: Learning rate for gradient descent.
+        @reg_const: The regularization constant which is used to control the regularization.
+        @num_iterations: Number of iterations over the training set.
+        @num_print: Number of print statements during iterations.
+        """
+
+        ''' START YOUR CODE HERE '''
+
+        # Initialise weights with normal distribution in the variable self.weights,
+        # while taking in consideration the bias (so two cases according to the boolean value of add_bias).
+
+        if self.add_bias:
+            x = np.random.randn(X_train.shape[1])
+            self.weights = np.append(x, 1)
+        else:
+            self.weights = np.random.randn(X_train.shape[1]) # Fill a correct value here
+
+        X_train_adj = None
+
+        # Create a new input array by adding bias column with all values one in X_train_adj array
+        # while taking in consideration the bias (so two cases according to the boolean value of add_bias).
+        if self.add_bias:
+            X_train_adj = np.hstack((X_train, np.ones((X_train.shape[0], 1))))  # Fill a correct value here
+        else:
+            X_train_adj = X_train
+
+        # Perform iterations, use output of loss function above to get unregularized loss and gradient
+        old_loss = 100000000
+        for i in range(num_iters):
+            loss, grad = self.loss(X_train_adj, Y_train)
+            # Add Regularisation to the loss and grad (remember to use the reg_const being passed to the function)
+            # See hint.pdf for the equations
+            
+            loss += (reg_const/(2*self.weights.shape[0])) * np.sum(self.weights**2)
+            #print(loss, old_loss)
+            if loss > old_loss:
+                lr = 0
+            old_loss = loss
+            #print(self.weights.shape)
+            grad += (reg_const/self.weights.shape[0]) * self.weights
+            # Update weights (remember to use the learning rate being passed to the function)
+            # See hint.pdf for equations
+            self.weights = self.weights - lr*(grad + (reg_const/self.weights.shape[0])*self.weights)
+            if i % (num_iters // num_print) == 0:
+                print("Iteration %d of %d. Loss: %f LR: %f" % (i, num_iters, loss, lr))
+
+        ''' END YOUR CODE HERE '''
